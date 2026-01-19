43ce5e4217a919369bcd4973b82014ba0ae59c9c
Pushpa <pushpa.nagaraj5252@gmail.com>
2026-01-19 23:55:21 +0530

    Initial AWS DevOps project

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+PORT = int(os.environ.get("PORT", 8080))
+
+@app.route("/")
+def home():
+    return "Hello from AWS DevOps Project 🚀"
+
+@app.route("/health")
+def health():
+    return {"status": "UP"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
